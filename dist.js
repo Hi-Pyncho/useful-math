2983963b52ac1be38169249db87f54ae1a8fd520
Anton Mikaskin <garrakombo@gmail.com>
2024-05-10 01:39:59 +0300

    refactor and add function to index

--- dist.js
+++
@@ -1,8 +0,0 @@
-/**
- * @param  {number} x1 x-coordinate of the first point.
- * @param  {number} y1 y-coordinate of the first point.
- * @param  {number} x2 x-coordinate of the second point.
- * @param  {number} y2 y-coordinate of the second point.
- * @return {number}    distance between the two points.
- */
-export default (x1, y1, x2, y2) => Math.hypot(x2 - x1, y2 - y1);;
--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 import constrain from './constrain.js';
-import dist from './dist.js';
 import lerp from './lerp.js';
 import map from './map.js';
 import noise from './noise.js';
 import random from './random.js';
 import getHeadingFromPosition from './getHeadingFromPosition.js';
 import getHeadingfromAngle from './getHeadingfromAngle.js';
+import getDistanceBetweenTwoPoints from './getDistanceBetweenTwoPoints.js';
+import checkAABB from './checkAABB.js';
 
 export {
   constrain,
-  dist,
   lerp,
   map,
   noise,
   random,
+  getHeadingfromAngle,
+  getHeadingFromPosition,
+  getDistanceBetweenTwoPoints,
+  checkAABB,
 };
